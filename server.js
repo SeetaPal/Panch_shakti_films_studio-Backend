b62f13c7d8b39a2e37d58cf869785d53f6580565
Seeta Pal <seetapal875@gmail.com>
2025-11-11 18:07:45 +0530

    only server part  i am work

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 
-
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const path = require("path");
+const cors = require('cors');
 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
+
+// ✅ Connect MongoDB
 connectDB();
 
+// ✅ API Routes
 app.use('/api/auth', authRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Panch Shakti Films Studio API 🎬');
+// ✅ Serve React build (static files)
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// ✅ For React Router: handle all other routes
+// app.get('/', (req, res) => {
+//   res.send('Welcome to Panch Shakti Films Studio API 🎬');
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
+
+// ✅ Server Port
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
